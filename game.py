6e079f85a4a2f21d8b57f5d76c7c6f0b07eeae12
Nolan Freeman <thenolanfreeman@icloud.com>
2020-04-22 14:02:46 -0500

    build game and main files to run game

--- game.py
+++ game.py
@@ -7,13 +7,25 @@ class Game:
         # enviroment setup variables
         self.window = {'width': 600, 'height': 600}
         self.entity_size = 20
-        self.boundries = {'left' : -1 * window_width/2 + entity_size/2,
-                          'right' : window_width/2 - entity_size/2,
-                          'top' : -1 * window_height/2 + entity_size/2,
-                          'bottom' : window_height/2 - entity_size/2}
+        self.boundries = {'left' : -1 * window['width']/2 + entity_size/2,
+                          'right' : window['width']/2 - entity_size/2,
+                          'top' : -1 * window['height']/2 + entity_size/2,
+                          'bottom' : window['height']/2 - entity_size/2}
+        self.theme = {'bgcolor': 'green'}
 
         # score gameplay variables
         self.high_score = 0
         self.score = 0
 
         self.state = states['play']
+
+    # getter methods
+    def get_window_dim():
+        return self.window
+
+    def get_theme():
+        return self.theme
+
+    # Where the whole game runs
+    def start():
+        pass
--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import turtle
 import time
 import random
+from game import Game
 
 delay = 0.1
 
+game = Game()
 
+window_dim = game.get_window_dim()
+theme = game.get_theme()
 
 # Set up the screen
-wn = turtle.Screen()
-wn.title("Snake Game by @TokyoEdTech")
-wn.bgcolor("green")
-wn.setup(width=window_width, height=window_height)
-wn.tracer(0) # Turns off the screen updates
+window = turtle.Screen()
+window.title("PySnake by @NolanFreeman")
+window.bgcolor(theme['bgcolor'])
+window.setup(window_dim['width'], window_dim['height'])
+window.tracer(0) # Turns off the screen updates
+
+game.start()
+
+
+
 
 # Pen
 pen = turtle.Turtle()
@@ -24,15 +33,15 @@ pen.goto(0, 260)
 pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
 
 # Keyboard bindings
-wn.listen()
-wn.onkeypress(go_up, "w")
-wn.onkeypress(go_down, "s")
-wn.onkeypress(go_left, "a")
-wn.onkeypress(go_right, "d")
+window.listen()
+window.onkeypress(go_up, "w")
+window.onkeypress(go_down, "s")
+window.onkeypress(go_left, "a")
+window.onkeypress(go_right, "d")
 
 # Main game loop
 while True:
-    wn.update()
+    window.update()
 
     if state == states['menu']:
         s = 1
@@ -126,4 +135,4 @@ while True:
 
     time.sleep(delay)
 
-wn.mainloop()
+window.mainloop()
